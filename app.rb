8c708657d0ff6c1223d91e2afa3341c29db040c9
HAGARIHAYATO <49360378+HAGARIHAYATO@users.noreply.github.com>
2020-07-30 06:16:39 +0900

    Update app.rb

--- app.rb
+++ app.rb
@@ -3,7 +3,15 @@ require 'sinatra/reloader'
 require 'json'
 
 get '/' do
-  params['key1'] ? params['key1'] : "hello"
+  "hello"
+end
+
+get '/pages/:number' do
+  "No. #{params['number']}"
+end
+
+get '/books/*' do |title|
+  title
 end
 
 get '/pages' do
@@ -11,12 +19,4 @@ get '/pages' do
   @key2 = params['key2'] ? params['key2'] : ""
   @rend = {keyA: @key1, keyB: @key2}
   @rend.to_json
-end
-
-get '/pages/:number' do
-  "Hello #{params['number']}"
-end
-
-get '/books/*' do |title|
-  title
 end
